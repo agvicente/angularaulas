23a464a401b2247ae7ce4f448c9f03eb2c52ed75
Augusto Custodio <custodioovicente@outlook.com.br>
2023-04-29 22:08:06 -0300

    Exemplo loop *ngFor

--- angularaulas/src/app/app.component.ts
+++ angularaulas/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html'
 })
 export class AppComponent {
   title = 'angularaulas';
+
+  nomes = [
+    {nome:'Augusto', idade:26},
+    {nome:'Paulo', idade:20},
+    {nome:'Fulano', idade:55}
+  ];
+
+  nomePrincipal = this.nomes[0];
 }
