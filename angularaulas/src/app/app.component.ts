e4f74a0b4a8c50ebd9b844f7c314335c6f7e8cdb
Augusto Custodio <custodioovicente@outlook.com.br>
2023-05-28 00:28:25 -0300

    Troca dos itens estáticos por objetos

--- angularaulas/src/app/app.component.ts
+++ angularaulas/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Proprietario } from './proprietario';
+import { ItemBlog } from './itemBlog';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-nome: string = 'Augusto Custódio Vicente';
-biografia: string = 'Sou nascido e criado em Belo horizonte, formado Engenharia Eletronica e de Telecomunicação pela Pontifícia Unicersidade Católica de minas Gerais. Atualmente faço pós graduação em Arquitetura de Software Distribuido na mesma instituição. A médio prazo pretendo me tornar lider técnico em uma empresa ou startup, e a longo prazo penso em empreender na área de tecnologia, agregando todo o conhecimento que tive nas formações. Durante o percurso pretendo também fazer um mestrado em Engenharia na Universidade Federal de Minas Gerais.';
+proprietario: Proprietario = new Proprietario(
+  'Augusto Custódio Vicente',
+  'Sou nascido e criado em Belo Horizonte, formado Engenharia Eletronica e de Telecomunicação pela Pontifícia Unicersidade Católica de minas Gerais. Atualmente faço pós graduação em Arquitetura de Software Distribuido na mesma instituição. A médio prazo pretendo me tornar lider técnico em uma empresa ou startup, e a longo prazo penso em empreender na área de tecnologia, agregando todo o conhecimento que tive nas formações. Durante o percurso pretendo também fazer um mestrado em Engenharia na Universidade Federal de Minas Gerais.'
+);
+
+itensBlog: ItemBlog[] = [
+  new ItemBlog('5 dicas para a sua carreira profissional', 
+  'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor'),
+  new ItemBlog('5 dicas para a sua carreira profissional', 
+  'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor'),
+  new ItemBlog('5 dicas para a sua carreira profissional', 
+  'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor')
+]
+
+ 
 }
---
+++ angularaulas/src/app/proprietario.ts
@@ -0,0 +1,9 @@
+export class Proprietario {
+    nome: string;
+    biografia: string;
+
+    constructor(nome: string, biografia: string){
+        this.nome = nome;
+        this.biografia = biografia;
+    }
+}
