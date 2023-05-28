fc77507269356b968aeaad1c2621f9d3a824c448
Augusto Custodio <custodioovicente@outlook.com.br>
2023-05-27 23:28:49 -0300

    Nome e biografia

--- angularaulas/src/app/app.component.ts
+++ angularaulas/src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-
+nome: string = 'Augusto Custódio Vicente';
+biografia: string = 'Sou nascido e criado em Belo horizonte, formado Engenharia Eletronica e de Telecomunicação pela Pontifícia Unicersidade Católica de minas Gerais. Atualmente faço pós graduação em Arquitetura de Software Distribuido na mesma instituição. A médio prazo pretendo me tornar lider técnico em uma empresa ou startup, e a longo prazo penso em empreender na área de tecnologia, agregando todo o conhecimento que tive nas formações. Durante o percurso pretendo também fazer um mestrado em Engenharia na Universidade Federal de Minas Gerais.';
 }
